e340338ec72a4c8c3d1a9719992091e30d54ad74
Blake Bartenbach <blakebartenbach@gmail.com>
2018-02-24 14:21:10 -0600

    Finished Assignment 2.  It's not pretty, but it works flawlessly.

--- assignment-2/wordsearch.c
+++ assignment-2/wordsearch.c
@@ -2,16 +2,43 @@
 #include <string.h>
 #include "wordsearch.h"
 
-
 int main() {
-    char puzzle[50][50] = {{ ' ' }};
-    char words[50] = { ' ' };
-    populateWordArray(puzzle, words);
-    printArray(puzzle);
+    char puzzle[100][100] = {{ ' ' }};
+    char solution[100][100] = {{ ' ' }};
+    char words[100][100];
+    int wordCount = populateWordArray(puzzle, words);
+    int puzzleLength;
+    populateSolutionArray(solution);
+    findSolution(puzzle, words, solution, wordCount);
+    puzzleLength = getPuzzleLength(puzzle);
+    printArray(solution, puzzleLength);
     return 0;
 }
 
-void populateWordArray(char wordArray[50][50], char words[50]) {
+int getPuzzleLength(char puzzle[100][100]) {
+    int i;
+    for (i = 0; i < 100; i++) {
+        if (puzzle[0][i] == ' ' || puzzle[0][i] == '\0') {
+            return i;
+        }
+    }
+    return 999;
+}
+
+void populateSolutionArray(char pString[100][100]) {
+    int i,j;
+    for (i = 0; i < 100; i++) {
+        for (j = 0; j < 100; j++) {
+            if (j == 49) {
+                pString[i][j] = '\n';
+            } else {
+                pString[i][j] = ' ';
+            }
+        }
+    }
+}
+
+int populateWordArray(char wordArray[100][100], char words[100][100]) {
     char string[100];
     int counter = 0;
     int wordCounter = 0;
@@ -27,6 +54,7 @@ void populateWordArray(char wordArray[50][50], char words[50]) {
                     continue;
                 /* if it's a null byte, the string has ended */
                 } else if (string[i] == '\0') {
+                    wordArray[counter][index] = '\0';
                     break;
                 } else {
                     wordArray[counter][index] = string[i];
@@ -35,38 +63,294 @@ void populateWordArray(char wordArray[50][50], char words[50]) {
             }
             counter++;
         } else {
-            words[wordCounter] = (char) string;
+            strcpy(words[wordCounter], string);
+            wordCounter++;
         }
     }
+    return wordCounter;
 }
 
-void printArray(char array[50][50]) {
+void printArray(char array[100][100], int length) {
     int row,col;
-    for (row = 0; row < 50; row++) {
+    for (row = 0; row < length; row++) {
         /* row contains no data */
         if (array[row][0] == '\0') {
             continue;
         }
-        for (col = 0; col < 50; col++) {
-            /* if it's a null byte, we've reached the end of the values here */
-            if (array[row][col] != '\0') {
-                /* if it's a newline character, don't print whitespace after it */
-                if (array[row][col] == '\n') {
-                    printf("\n");
-                    break;
-                }
-                printf("%c ", array[row][col]);
-            } else {
+        for (col = 0; col < 100; col++) {
+            if (array[row][col] != ' ' && array[row][col] != '\n') {
+                printRow(array, row, length);
                 break;
+            }
+
+        }
+    }
+}
+
+void printRow(char array[100][100], int row, int colSize) {
+    int col;
+    for (col = 0; col < colSize - 1; col++) {
+        /* if it's a null byte, we've reached the end of the values here */
+        if (array[row][col] != '\0') {
+            /* if it's a newline character, don't print whitespace after it */
+            if (array[row][col] == '\n') {
+                printf("\n");
+                break;
+            }
+            printf("%c ", array[row][col]);
+        } else {
+            break;
+        }
+    }
+    printf("\n");
+}
+
+void findSolution(char puzzle[100][100], char words[100][100], char solution[100][100], int wordCount) {
+    int row, col;
+    for (row = 0; row < 51; row++) {
+        /* row is empty */
+        if (puzzle[row][0] == '\0') {
+            continue;
+        }
+        for (col = 0; col < 51; col++) {
+            if (puzzle[row][col] == '\0' || puzzle[row][col] == '\n') {
+                break;
+            } else {
+                checkStartingPoint(puzzle, row, col, words, solution, wordCount);
             }
         }
     }
 }
 
-void printWords(char words[50]) {
+void checkStartingPoint(char puzzle[100][100], int row, int col, char words[100][100], char solution[100][100], int wordCount) {
     int i = 0;
-    for (i = 0; i < sizeof(words); i++) {
-       printf(words[i]);
+    /* end of row */
+        for (i=0; i < wordCount; i++) {
+            if (puzzle[row][col] == words[i][0]) {
+                checkForwards(puzzle, words[i], row, col, solution);
+                checkBackwards(puzzle, words[i], row, col, solution);
+                checkDown(puzzle, words[i], row, col, solution);
+                checkUp(puzzle, words[i], row, col, solution);
+                checkUpRight(puzzle, words[i], row, col, solution);
+                checkUpLeft(puzzle, words[i], row, col, solution);
+                checkDownRight(puzzle, words[i], row, col, solution);
+                checkDownLeft(puzzle, words[i], row, col, solution);
+            }
+        }
+
+}
+
+/* verified */
+int checkUpLeft(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        if (puzzle[row-i][col-i] == word[i]) {
+            coordinates[i].x = row-i;
+            coordinates[i].y = col-i;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+int checkDownRight(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        if (puzzle[row+i][col+i] == '\0' || puzzle[row+i][col+i] == '\n') {
+            return 0;
+        }
+        if (puzzle[row+i][col+i] == word[i]) {
+            coordinates[i].x = row+i;
+            coordinates[i].y = col+i;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+int checkDownLeft(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        if (puzzle[row+i][col-i] == '\0' || puzzle[row+i][col-i] == '\n') {
+            return 0;
+        }
+        if (puzzle[row+i][col-i] == word[i]) {
+            coordinates[i].x = row+i;
+            coordinates[i].y = col-i;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+int checkUpRight(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        if (puzzle[row-i][col+i] == '\0' || puzzle[row-i][col+i] == '\n') {
+            return 0;
+        }
+        if (puzzle[row-i][col+i] == word[i]) {
+            coordinates[i].x = row-i;
+            coordinates[i].y = col+i;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+int checkBackwards(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        if (puzzle[row][col-i] == '\0' || puzzle[row][col-i] == '\n') {
+            return 0;
+        }
+        if (puzzle[row][col-i] == word[i]) {
+            coordinates[i].x = row;
+            coordinates[i].y = col-i;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+
+int checkDown(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        if (puzzle[row+i][col] == ' ') { /* end of puzzle */
+            return 0;
+        }
+        if (puzzle[row+i][col] == word[i]) {
+            coordinates[i].x = row+i;
+            coordinates[i].y = col;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+int checkUp(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        /*if (puzzle[row-1][col] == '\0' || puzzle[row][col] == '\n') {
+            return 0;
+        }*/
+        if (puzzle[row-i][col] == word[i]) {
+            coordinates[i].x = row-i;
+            coordinates[i].y = col;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+
+int checkForwards(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]) {
+    int i;
+    struct Coordinate startingPoint;
+    struct Coordinate coordinates[100];
+    startingPoint.x = row;
+    startingPoint.y = col;
+    coordinates[0] = startingPoint;
+    for (i = 1; i < 100; i++) {
+        if (puzzle[row][col+i] == '\0' || puzzle[row][col] == '\n') {
+            return 0;
+        }
+        if (puzzle[row][col+i] == word[i]) {
+            coordinates[i].x = row;
+            coordinates[i].y = col+i;
+            if (word[i+1] == '\n') {
+                copyCoordinates(puzzle, coordinates, solution, (int) strlen(word+1));
+                return 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
+
+void copyCoordinates(char puzzle[100][100], struct Coordinate coordinates[100], char solution[100][100], int length) {
+    int i;
+    for (i = 0; i < length; i++) {
+        solution[coordinates[i].x][coordinates[i].y] = puzzle[coordinates[i].x][coordinates[i].y];
+    }
+}
+
+void printWords(char words[100][100], int wordCount) {
+    int i = 0;
+    for (i = 0; i < wordCount; i++) {
+        printf("%s", words[i]);
     }
 }
 
--- assignment-2/wordsearch.h
+++ assignment-2/wordsearch.h
@@ -3,6 +3,24 @@
 
 #endif
 
-void populateWordArray(char[50][50], char[50]);
-void printArray(char[50][50]);
-void printWords(char[50]);
+int populateWordArray(char[100][100], char[100][100]);
+void printArray(char[100][100], int);
+void printRow(char array[100][100], int, int);
+void printWords(char[100][100], int);
+void findSolution(char[100][100], char[100][100], char[100][100], int);
+void checkStartingPoint(char[100][100], int, int, char[100][100], char[100][100], int);
+int checkForwards(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int checkBackwards(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int checkDown(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int checkUp(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int checkUpRight(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int checkUpLeft(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int checkDownRight(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int checkDownLeft(char puzzle[100][100], char word[100], int row, int col, char solution[100][100]);
+int getPuzzleLength(char puzzle[100][100]);
+struct Coordinate {
+    int x;
+    int y;
+};
+void copyCoordinates(char puzzle[100][100], struct Coordinate coordinates[100], char solution[100][100], int length);
+void populateSolutionArray(char pString[100][100]);
